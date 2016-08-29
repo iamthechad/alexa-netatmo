9f626038481a3628a5bc2713586e895af82a1c74
Chad Johnston <cjohnston@megatome.com>
2016-08-28 19:07:28 -0600

    Add cleanup gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
+var del = require('del');
 var zip = require('gulp-zip');
 
-gulp.task('default', function() {
-  return gulp.src('./**')
+gulp.task('default', ['clean:dist', 'build:zip']);
+
+gulp.task('build:zip', function() {
+  return gulp.src(['./**', '!./dist'])
     .pipe(zip('netatmo.zip'))
     .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean:dist', function () {
+  return del(['./dist']);
 });
